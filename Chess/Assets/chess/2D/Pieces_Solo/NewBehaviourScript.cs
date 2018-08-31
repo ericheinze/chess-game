5d9322938850fd06e1bf1344990598efe2420d98
Eric <eheil920@gmail.com>
2018-08-30 19:33:16 -0600

    Added Game Files

---
+++ Chess/Assets/chess/2D/Pieces_Solo/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public GameObject wPawn1;
+
+    // Use this for initialization
+    void Start () {
+
+        wPawn1 = GameObject.Find("W_Pawn_2D (8)");
+        Debug.Log ("Hello World");
+
+        wPawn1.transform.position = new Vector3(10, 10, 0);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Debug.Log(wPawn1.transform.position);
+        
+
+
+    }
+}
